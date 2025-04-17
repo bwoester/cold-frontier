806b92fe33af44cab49f5f7d495a292612683dfa
Benjamin Wöster <benjamin.woester@gmail.com>
2025-04-17 22:45:45 +0200

    Refactor CreateBuildingProgressMsg to add consumed flag
    Introduced a new `consumed` flag to track message consumption status. Added a constructor with default `consumed` value set to false for flexibility in message creation. This enhances clarity and extensibility of progress messages.

--- lib/src/main/java/de/bwoester/coldfrontier/progress/CreateBuildingProgressMsg.java
+++ lib/src/main/java/de/bwoester/coldfrontier/progress/CreateBuildingProgressMsg.java
@@ -2,5 +2,21 @@ package de.bwoester.coldfrontier.progress;
 
 import de.bwoester.coldfrontier.buildings.Building;
 
-public record CreateBuildingProgressMsg(Building building, double timeToBuildMultiplier, float progress) implements ProgressMsg {
+public record CreateBuildingProgressMsg(
+    Building building, 
+    double timeToBuildMultiplier, 
+    double progress, 
+    boolean consumed
+) implements ProgressMsg {
+    
+    /**
+     * Creates a new building progress message with unconsumed status.
+     * 
+     * @param building the building being constructed
+     * @param timeToBuildMultiplier multiplier for build time
+     * @param progress current progress value (0.0 to 1.0)
+     */
+    public CreateBuildingProgressMsg(Building building, double timeToBuildMultiplier, double progress) {
+        this(building, timeToBuildMultiplier, progress, false);
+    }
 }
